2753ccb5762ae27d9c45b2097d8e16bba3323884
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-05-13 22:28:13 -0600

    feat: read paths from yaml file

--- url_shortener/main.go
+++ url_shortener/main.go
@@ -1,35 +1,37 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/gophercises/url_shortener/handler"
 )
 
 func main() {
 	mux := defaultMux()
+	var file string
 
-	// build the mapHandler with mux as the fallback
-	// pathsToURls := map[string]string{
-	// 	"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-	// 	"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	// }
+	// parse flags
+	flag.StringVar(&file, "yaml", "paths.yaml", "path to yaml file")
+	flag.Parse()
 
-	yaml := `
-- path: /urlshort-godoc
-  url: https://github.com/gophercises/urlshort
-- path: /yaml-godoc
-  url: https://godoc.org/gopkg.in/yaml.v2
-`
-
+	// read file
+	yaml, err := readFile(file)
+	if err != nil {
+		panic(err)
+	}
 	// mapHandler := handler.MapHandler(pathsToURls, mux)
-	yamlHandler, err := handler.YamlHandler([]byte(yaml), mux)
+	yamlHandler, err := handler.YamlHandler(yaml, mux)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("server running on part :8080")
+	fmt.Printf("[>] reading paths from file: %s", file)
+	fmt.Println("[>] server running on part :8080")
 	http.ListenAndServe(":8080", yamlHandler)
 }
 
@@ -43,4 +45,20 @@ func defaultMux() *http.ServeMux {
 
 func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Gophercise 2 - URL shortener")
+}
+
+func readFile(path string) ([]byte, error) {
+	// validate file path
+	ok := strings.HasSuffix(path, ".yaml")
+	if !ok {
+		return nil, errors.New("Invalid file. File must have a .yaml extension")
+	}
+	// read yaml file
+	yamlFile, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return yamlFile, nil
 }
